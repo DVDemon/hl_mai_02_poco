c16f9de97f4172c1804d2cfe8d148a9dc4e2d461
Dmitriy Dzyuba <ddzuba@yandex.ru>
2022-03-10 13:53:01 +0000

    change url

--- poco_socket.cpp
+++ poco_socket.cpp
@@ -6,13 +6,11 @@
 
 int main()
 {
-    Poco::Net::SocketAddress sa("ptsv2.com", 80);
+    Poco::Net::SocketAddress sa("developer.mozilla.org", 80);
     Poco::Net::StreamSocket socket(sa);
 
     Poco::Net::SocketStream str(socket);
-    str << "GET / HTTP/1.0\r\n" <<
-           "Host: ptsv2.com\r\n" <<
-           "\r\n" <<
+    str << "GET / HTTP/1.1\r\n" <<
            "\r\n";
     str.flush();
 
